1159cb5553c85b9eef14cba6f18a0587a91d744e
hariharan23101997 <frise16@gmail.com>
2021-12-31 16:52:19 +0530

    Username Functionality

---
+++ src/test/java/org/com/LibGlobal1.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class LibGlobal1 {
+	
+	public static void username() {
+		System.out.println("Username");
+	}
+
+	public static void main(String[] args) {
+         username();
+         
+		
+	}
+
+}
